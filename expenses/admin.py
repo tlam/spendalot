a1e6337a2f6bac26671da4b1a6f60897515ca466
Thierry <lamthierry@gmail.com>
2014-08-10 23:41:31 -0400

    Adding expense search field

--- expenses/admin.py
+++ expenses/admin.py
@@ -5,5 +5,6 @@ from expenses.models import Expense
 
 class ExpenseAdmin(admin.ModelAdmin):
     list_display = ('description', 'category', 'date', 'amount',)
+    search_fields = ['description']
 
 admin.site.register(Expense, ExpenseAdmin)
