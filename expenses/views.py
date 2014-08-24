59664b0d10598a29ae5792e38f3da18fd9053469
Thierry <lamthierry@gmail.com>
2014-08-23 22:09:21 -0400

    Adding expense create functionality

--- expenses/views.py
+++ expenses/views.py
@@ -1,8 +1,9 @@
 from datetime import datetime
 import json
 
+from django.contrib import messages
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 
 from expenses.forms import ExpenseForm
 from expenses.models import Expense
@@ -26,7 +27,11 @@ def create(request):
     if request.method == 'POST':
         form = ExpenseForm(request.POST)
         if form.is_valid():
-            pass
+            expense = form.save()
+            expense.payment = 'CA'
+            expense.save()
+            messages.success(request, 'Expense created')
+            return redirect('expenses:create')
     else:
         form = ExpenseForm()
     context = {
@@ -42,7 +47,7 @@ def create(request):
 def descriptions(request):
     keyword = request.GET.get('term', '')
     if keyword:
-        expenses = Expense.cached().filter(description__icontains=keyword)
+        expenses = Expense.cached().filter(description__icontains=keyword).order_by('description').distinct('description')
         data = [expense.description for expense in expenses]
     else:
         data = []
