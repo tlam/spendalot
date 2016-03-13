3d6e7be1dbd3dd425ea9440f4dc17f8761c9931a
Thierry <lamthierry@gmail.com>
2016-03-13 16:08:56 -0400

    Handle the case where scikit is not available

--- data_sources/learn.py
+++ data_sources/learn.py
@@ -7,44 +7,49 @@ try:
     from sklearn.linear_model import SGDClassifier
     from sklearn.pipeline import Pipeline
     import pandas as pd
+
+
+    class Learn(object):
+
+        def __init__(self, refresh=False):
+            file_name = 'expenses.csv'
+            if not os.path.isfile(file_name) or refresh:
+                from expenses.models import Expense
+                with open(file_name, 'wb') as csvfile:
+                    writer = csv.writer(csvfile)
+                    writer.writerow(['Name', 'Category'])
+                    for expense in Expense.objects.all():
+                        writer.writerow([expense.description.encode('utf-8'), expense.category.name])
+
+            df = pd.read_csv(file_name)
+            data = df.Name.tolist()
+            categories = df.Category.tolist()
+            self.unique_categories = df.Category.unique().tolist()
+            self.unique_categories.sort()
+            le = preprocessing.LabelEncoder()
+            le.fit(categories)
+            target = le.transform(categories)
+
+            self.text_clf = Pipeline([
+                ('vect', CountVectorizer()),  # Bag of words
+                ('tfidf', TfidfTransformer()),  # term frequency-inverse document frequency
+                ('clf', SGDClassifier(loss='hinge', penalty='l2', alpha=1e-3, n_iter=5, random_state=42)),  # SVM classifier
+            ])
+
+            self.text_clf = self.text_clf.fit(data, target)
+
+        def predict(self, description):
+            search_terms = [description]
+            predicted = self.text_clf.predict(search_terms)
+
+            for search, category in zip(search_terms, predicted):
+                return self.unique_categories[category]
+
+            return ''
+
 except ImportError:
-    pass
+    
+    class Learn(object):
 
-
-class Learn(object):
-
-    def __init__(self, refresh=False):
-        file_name = 'expenses.csv'
-        if not os.path.isfile(file_name) or refresh:
-            from expenses.models import Expense
-            with open(file_name, 'wb') as csvfile:
-                writer = csv.writer(csvfile)
-                writer.writerow(['Name', 'Category'])
-                for expense in Expense.objects.all():
-                    writer.writerow([expense.description.encode('utf-8'), expense.category.name])
-
-        df = pd.read_csv(file_name)
-        data = df.Name.tolist()
-        categories = df.Category.tolist()
-        self.unique_categories = df.Category.unique().tolist()
-        self.unique_categories.sort()
-        le = preprocessing.LabelEncoder()
-        le.fit(categories)
-        target = le.transform(categories)
-
-        self.text_clf = Pipeline([
-            ('vect', CountVectorizer()),  # Bag of words
-            ('tfidf', TfidfTransformer()),  # term frequency-inverse document frequency
-            ('clf', SGDClassifier(loss='hinge', penalty='l2', alpha=1e-3, n_iter=5, random_state=42)),  # SVM classifier
-        ])
-
-        self.text_clf = self.text_clf.fit(data, target)
-
-    def predict(self, description):
-        search_terms = [description]
-        predicted = self.text_clf.predict(search_terms)
-
-        for search, category in zip(search_terms, predicted):
-            return self.unique_categories[category]
-
-        return ''
+        def predict(self, description):
+            return ''
